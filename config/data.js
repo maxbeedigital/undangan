5e385dc1e0cfb36b16284952cf8268190e671f12
maxbeedigital <maxbeefortv@gmail.com>
2026-07-06 22:15:17 +0700

    Update data.js

--- config/data.js
+++ config/data.js
@@ -1,1 +1,67 @@
+const wedding = {
+  groom: {
+    fullName: "Misbahul Munir",
+    nickName: "Munir",
+    father: "Samsudin",
+    mother: "Siti Romdonah",
+    address: "Ds Gempoldenok RT 02 RW 01, Kec. Dempet, Kab. Demak"
+  },
 
+  bride: {
+    fullName: "Della Nur Cahyani",
+    nickName: "Della",
+    father: "Iskak Udin",
+    mother: "Sulis Tyo Wati",
+    address: "Ds Brakas Gg 8 RT 03 RW 02, Kec. Dempet, Kab. Demak"
+  },
+
+  wedding: {
+    title: "The Wedding Of",
+    date: "24 Juli 2026",
+    akad: {
+      day: "Jumat",
+      date: "24 Juli 2026",
+      time: "08.00 WIB",
+      place: "Kediaman Mempelai Wanita"
+    },
+    reception: {
+      day: "Sabtu",
+      date: "25 Juli 2026",
+      time: "Bebas",
+      place: "Kediaman Mempelai Pria"
+    }
+  },
+
+  contact: {
+    whatsapp: "6287733833331",
+    maps: "https://maps.app.goo.gl/3PouQDLxATP8c9RQ9"
+  },
+
+  bank: [
+    {
+      name: "BRI",
+      number: "592901014446536",
+      owner: "MISBAHUL MUNIR"
+    },
+    {
+      name: "BNI",
+      number: "0448193253",
+      owner: "MISBAHUL MUNIR"
+    },
+    {
+      name: "BCA",
+      number: "8365154542",
+      owner: "MISBAHUL MUNIR"
+    },
+    {
+      name: "Mandiri",
+      number: "1350023119421",
+      owner: "MISBAHUL MUNIR"
+    },
+    {
+      name: "DANA / OVO / GoPay / ShopeePay",
+      number: "08989756416",
+      owner: "MISBAHUL MUNIR"
+    }
+  ]
+};
