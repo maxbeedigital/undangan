4295e37ce147fa73fc45ce6d585d4b1671c240ed
maxbeedigital <maxbeefortv@gmail.com>
2026-07-06 21:50:26 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+const btn=document.getElementById("openInvitation");
+const cover=document.getElementById("cover");
 
+btn.addEventListener("click",()=>{
+
+cover.style.opacity="0";
+
+cover.style.transition=".8s";
+
+setTimeout(()=>{
+
+cover.style.display="none";
+
+},800);
+
+});
